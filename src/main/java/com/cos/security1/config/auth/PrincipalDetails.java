155fa486f8ae6069ecb91161b20b59f6805a0265
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-15 16:59:06 +0900

    feat. Authentication 객체가 가질 수 있는 2가지 타입

--- src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
@@ -11,16 +11,23 @@ package com.cos.security1.config.auth;
 import com.cos.security1.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user; // 콤포지션
 
     public PrincipalDetails(User user){
         this.user = user;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
     }
 
     // 해당 User의 권한을 리턴하는 곳!!
@@ -65,5 +72,10 @@ public class PrincipalDetails implements UserDetails {
     public boolean isEnabled() {
         // 우리 사이트. 1년 동안 회원이 로그인을 사용하지 않는다면 휴면 계정으로 전환.
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
