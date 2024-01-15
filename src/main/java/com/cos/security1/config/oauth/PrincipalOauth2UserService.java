67d81e6372d06f980f0861afe5593d686f3d74a5
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-15 16:51:32 +0900

    feat. 구글 회원 프로필 정보 받아오기

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -11,11 +11,16 @@ import org.springframework.stereotype.Service;
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
     @Override
-    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest){
+    public OAuth2User loadUser(OAuth2UserRequest userRequest){
         //accessToken으로 서드파티에 요청하여 사용자 정보를 얻어옴
         log.debug("--".repeat(10) +"loadUser 메서드 호출!!!!" );
-        System.out.println(oAuth2UserRequest);
-        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
-        return null;
+        System.out.println(userRequest);
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println("getClientRegistration : " + userRequest.getClientRegistration());
+        System.out.println("getAccessToken: " + userRequest.getAccessToken().getTokenValue());
+        System.out.println("getAttributes: " + oAuth2User.getAttributes());
+
+        // 회원 가입을 강제로 진행할 예정
+        return oAuth2User;
     }
 }
